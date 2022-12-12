e0687e51ce7af4b6a616dfd0351505fb12e6a72e
KYEONGMIN CHO <ggingmin@gmail.com>
2022-12-12 17:25:55 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+  hello: () => {
+    return 'Hello, Cloudtype!';
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('http://localhost:4000/graphql 에서 GraphQL API 서버가 구동 중입니다.');
