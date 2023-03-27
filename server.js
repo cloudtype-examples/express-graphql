e7085047b6d7cd6505b01a2a9d78ff2097485a58
KYEONGMIN CHO <ggingmin@gmail.com>
2023-03-27 13:39:42 +0900

    index page added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var path = require('path');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+var nunjucks = require('nunjucks');
 
 var schema = buildSchema(`
   type Query {
@@ -14,11 +16,21 @@ var root = {
   },
 };
 
+var indexRouter = require('./routes/index');
+
 var app = express();
 
-app.get('/',(req, res) => {
-  res.send('Please access to /graphql')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'njk');
+nunjucks.configure('views', { 
+  express: app,
+  watch: true,
 });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
